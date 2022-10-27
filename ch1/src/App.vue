8ca711561dfc034c6f8f7dbfd922eca9268f0f1e
hitesh-sysorex <hiteshsysorex@gmail.com>
2022-10-27 08:55:25 +0530

    first commit

---
+++ ch1/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import HelloWorld from './components/HelloWorld.vue';
+
+const name = 'Sagufta'
+</script>
+
+<template>
+ <div><h1>Local Registrarion created by {{name}}</h1></div>
+ <HelloWorld/>
+</template>
+
+<style scoped>
+h1{
+  font-size: 90px;
+}
+</style>
---
+++ ch1/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+// const name = 'Sagufta'
+</script>
+
+<template>
+    <div>
+        <h1>Hello how are you</h1>
+    </div>
+
+</template>
+
+<style scoped>
+/* h1 {
+    font-size: 90px;
+} */
+</style>
